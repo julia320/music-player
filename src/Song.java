e8f96a1986ce0a3b192afa5b2fd3151a58e58181
Julia Bristow <julia_bristow@gwu.edu>
2018-04-24 23:02:57 -0400

    add getter and setter methods

--- src/Song.java
+++ src/Song.java
@@ -1,17 +1,23 @@
-/* Author: Julia Bristow
- * CS 2113
+/* Authors: Julia Bristow and Neel Bhagwat
+ * Song class
+ * Purpose: to make a song class that stores cover art, title, artist, album, and songData.
+ * Plays the specified song. 
  * 
- * Song class for use in a file-based
- * music playing program
  */
+
+import java.awt.*;
+import javax.swing.ImageIcon;
+import jm.util.*;
 
 public class Song
 {
-	String title;
-	String artist;
-	String album;
-	String songPath;
-	String imagePath;
+	private String title;
+	private String artist;
+	private String album;
+	private String songPath;
+	private float[] songData;
+	private String imagePath;
+	private Image image;
 	
 	// constructor
 	public Song (String title, String artist, String album, String songPath, String imagePath)
@@ -21,5 +27,64 @@ public class Song
 		this.album = album;
 		this.songPath = songPath;
 		this.imagePath = imagePath;
+		this.songData = Read.audio(songPath);
+		this.image = new ImageIcon(imagePath).getImage();
+	}
+	
+	// getter and setter methods
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	
+	public String getArtist() {
+		return artist;
+	}
+	
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+	
+	public String getAlbum() {
+		return album;
+	}
+	
+	public void setSongPath(String songPath) {
+		this.songPath = songPath;
+	}
+	
+	public String getSongPath() {
+		return songPath;
+	}
+	
+	public void setSongData(float[] songData) {
+		this.songData = songData;
+	}
+	
+	public float[] getSongData() {
+		return songData;
+	}
+		
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+	
+	public String getImagePath() {
+		return imagePath;
+	}
+	
+	public void setImage(Image image) {
+		this.image = image;
+	}
+	
+	public Image getImage() {
+		return image;
 	}
 }
