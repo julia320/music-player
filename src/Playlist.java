e9d9ff9bc3b34ab646fdeccfd27c81fcc149f702
Julia Bristow <31576485+julia320@users.noreply.github.com>
2018-04-27 15:21:34 -0400

    remove playAll function

---
+++ src/Playlist.java
@@ -0,0 +1,46 @@
+package finalproject;
+
+/* Author: Julia Bristow
+ * CS 2113
+ * 
+ * Linked list of songs for use in a file-based
+ * music playing program
+ */
+
+import jm.util.*;
+
+public class Playlist
+{
+	ListNode head;
+	ListNode tail; 
+	int size; 
+	
+	// add a song to the end of the list
+	void add (Song song)
+	{
+		if (size!=0)
+		{
+			// make new node with song information
+			ListNode node = new ListNode(song);
+			// make the node after tail the new node
+			tail.next = node;
+			tail.next.song = song;
+			// have the new node's prev point to the tail 
+			node.prev = tail;
+			// set the tail to the new node at the end
+			tail = tail.next;
+			// increase size by 1
+			size++;
+		}
+		else // if list is empty
+		{
+			// make new node at the head
+			head = new ListNode(song);
+			head.song = song;
+			// set tail equal to head since there is only 1 node
+			tail = head;
+			// increase size by 1
+			size++;
+		}
+	} 
+}
